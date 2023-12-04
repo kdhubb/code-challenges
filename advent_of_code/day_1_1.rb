6733cb623c33baa0ed3b9a7ffbc8111d76562dfb
K.D. Hubbard <hubbard.kd@gmail.com>
2023-12-04 12:07:39 -0700

    wip number too high

--- advent_of_code/day_1_1.rb
+++ advent_of_code/day_1_1.rb
@@ -48,7 +48,7 @@
 
 def sum_calibration(text_file)
   sum = 0
-  nums = { "one"=> "1", "two"=> "2", "three"=> "3",
+  nums = { "zero"=> "0", "one"=> "1", "two"=> "2", "three"=> "3",
             "four"=> "4", "five"=> "5", "six"=> "6",
             "seven"=> "7", "eight"=> "8", "nine"=> "9" }
   re = Regexp.union(nums.keys)          
