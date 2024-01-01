cfd1cbd73ddc36ed4f528aeae9999a6c39f573eb
Robert Williamson <williamson.robert@gmail.com>
2024-01-01 15:51:27 +0000

    Day 2 part 1 done.

--- advent_of_code/src/day2.rs
+++ advent_of_code/src/day2.rs
@@ -1,9 +1,86 @@
 pub fn run() {
-    println!("Hello world!");
+    let file = std::fs::read_to_string("02-input.txt").unwrap();
+    println!("{}", exec(&file));
+}
+
+fn exec(file: &str) -> i32 {
+    let games = process(file);
+    let red = 12;
+    let green = 13;
+    let blue = 14;
+
+    let mut sum = 0;
+    for game in games {
+        if game.max_red <= red && game.max_green <= green && game.max_blue <= blue {
+            sum += game.number
+        }
+    }
+
+    sum
+}
+
+#[derive(Debug)]
+struct Game {
+    number: i32,
+    max_red: i32,
+    max_blue: i32,
+    max_green: i32,
+}
+
+fn process(file: &str) -> Vec<Game> {
+    let file = file.trim();
+
+    let mut lines = file.lines();
+    let mut games = Vec::new();
+    while let Some(line) = lines.next() {
+        let (game, cubes) = line.split_once(':').unwrap();
+
+        let game = game.split_at("Game ".len()).1.parse::<i32>().unwrap();
+
+        let cube_sets = cubes.split(';');
+        let mut max_red = i32::MIN;
+        let mut max_blue = i32::MIN;
+        let mut max_green = i32::MIN;
+        for cube_set in cube_sets {
+            let cubes = cube_set.split(',');
+            for cube in cubes {
+                let cube = cube.trim();
+                let (number, color) = cube.split_once(' ').unwrap();
+                let number = number.parse::<i32>().unwrap();
+                match color {
+                    "red" => max_red = max_red.max(number),
+                    "green" => max_green = max_green.max(number),
+                    "blue" => max_blue = max_blue.max(number),
+                    _ => panic!("Unknown color"),
+                }
+            }
+        }
+
+        games.push(Game {
+            number: game,
+            max_red: max_red,
+            max_green: max_green,
+            max_blue: max_blue,
+        });
+    }
+
+    games
 }
 
 #[cfg(test)]
 mod test {
+    use super::*;
+
+    const FILE: &str = r#"
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+"#;
+
     #[test]
-    fn part1() {}
+    fn part1() {
+        assert_eq!(exec(FILE), 8);
+    }
 }
