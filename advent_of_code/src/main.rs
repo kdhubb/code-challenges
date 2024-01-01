386151c52bcce603dc2189997bd363e0a1b9671e
Robert Williamson <williamson.robert@gmail.com>
2024-01-01 14:43:20 +0000

    Split rust code to allow running multiple days from a single file.

--- advent_of_code/src/main.rs
+++ advent_of_code/src/main.rs
@@ -1,3 +1,18 @@
+mod day2;
+
 fn main() {
-    println!("Hello, world!");
+    let arg_matches = clap::Command::new("advent_of_code")
+        .arg(
+            clap::Arg::new("day")
+                .value_parser(clap::value_parser!(u8).range(1..31))
+                .required(true),
+        )
+        .get_matches();
+
+    let day: &u8 = arg_matches.get_one("day").unwrap();
+    match day {
+        1 => println!("Day 1 was written in Go."),
+        2 => day2::run(),
+        _ => panic!("invalid day"),
+    }
 }
